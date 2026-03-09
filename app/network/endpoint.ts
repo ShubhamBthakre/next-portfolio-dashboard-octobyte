de6fbbb277fc5eb54ba43c765a13a31925e28db8
ShubhamBthakre <shubham.thakre31@gmail.com>
2026-03-09 23:18:36 +0530

    changed the endpoint with https

--- app/network/endpoint.ts
+++ app/network/endpoint.ts
@@ -1,3 +1,3 @@
 // const BASE_URL = "http://localhost:5000/api";
-const BASE_URL = "http://13.126.84.246:5000/api";
+const BASE_URL = "https://13.126.84.246:5000/api";
 export const PORTFOLIO_API_URL = `${BASE_URL}/portfolio`;
