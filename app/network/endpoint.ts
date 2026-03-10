89b8e088cdf55dee7a9456b74a71ef033ef0b2ae
ShubhamBthakre <shubham.thakre31@gmail.com>
2026-03-10 14:35:52 +0530

    fix types issue

--- app/network/endpoint.ts
+++ app/network/endpoint.ts
@@ -1,3 +1,4 @@
 // const BASE_URL = "http://localhost:5000/api";
-const BASE_URL = "https://13.126.84.246:5000/api";
+// const BASE_URL = "https://13.126.84.246:5000/api";
+const BASE_URL = "https://node-portfolio-dashboard-octobyte.onrender.com/api";
 export const PORTFOLIO_API_URL = `${BASE_URL}/portfolio`;
